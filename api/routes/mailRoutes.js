9846a30cac7bd6a6837aea8a662a9d2df164f056
Mario César <macer1991@gmail.com>
2020-11-18 16:41:47 -0500

    Add send email custom

--- api/routes/mailRoutes.js
+++ api/routes/mailRoutes.js
@@ -1,9 +1,12 @@
 'use strict';
-module.exports = function(app) {
-  
+module.exports = function (app) {
+
   var MailController = require('../controllers/mailController');
 
   app.route('/send-email')
     .post(MailController.sendMail);
+
+  app.route('/send-email-custom')
+    .post(MailController.sendMailCustom);
 
 };
